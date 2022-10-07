f8607bca7f0f15db7725bfb3fb258ba32a12a31a
Muhammad Abduttawwab <111693086+Muhat123@users.noreply.github.com>
2022-10-07 12:30:58 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div></div>
+  <HelloWorld />
+  <div class="container">
+    <div class="row">
+      <div class="col-8">
+        <ProductsList />
+        <div class="col-4">
+          <CartNew />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ProductsList from './components/ProductsList.vue'
+import CartNew from './components/CartNew.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ProductsList,
+    CartNew,
+},
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c4763;
+}
+</style>
---
+++ ProductsList.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="card">
+        v-for="item in products" :key="item.id">
+  <h4 class="card-header">Products</h4>
+  <div class="card-body">
+    <p class="card-title">Silver Queen</p>
+    <p class="card-cost">$0.95</p>
+    <a href="#" @click="addToCart(item)" class="btn btn-primary">Add to cart</a>
+  </div>
+</div>
+  <div class="card">
+  <div class="card-body">
+    <p class="card-title">Snickers</p>
+    <p class="card-cost">$0.99</p>
+    <a href="#" @click="addToCart(item)" class="btn btn-primary">Add to cart</a>
+  </div>
+</div>
+  <div>
+  <div class="card">
+  <div class="card-body">
+    <p class="card-title"></p>
+    <p class="card-text">Dairy Milk</p>
+    <p class="card-cost">$1</p>
+    <a href="#" @click="addToCart(item)" class="btn btn-primary">Add to cart</a>
+  </div>
+</div>
+</div>
+<div>
+</div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+    export default {
+        name: "ListProducts",
+        computed: {
+            ...mapGetters (["products"]),
+        },
+        methods: {
+            ...mapActions (["getProducts", "addToCart"]),
+        },
+        mounted() {
+            this.getProducts();
+        }
+    };
+</script>
+<style scoped>
+    .card {
+        padding-inline-end: 0px;
+    }
+</style>
